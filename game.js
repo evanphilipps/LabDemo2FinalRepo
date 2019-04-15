dd54d8cbc188fd8df44e34f8d2078c479ae37736
evanphilipps <eaphilipps@gmail.com>
2019-04-14 21:47:26 -0400

    fixed magnitude of bugs - timer, guessing, round endings, score, functionality

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
-var counter = 0;
-var timeleft = 120;
+var counter;
+var timeleft = 15;
+var pressed = false;
 var tenSecondsLeft;
 var timesup;
 var randomWord;
@@ -13,6 +14,9 @@ var word;
 var points;
 var colorNow;
 var assert;
+var words = ['cheese', 'bone', 'socks', 'leaf', 'whale', 'pie', 'shirt', 'orange', 'lollipop', 'bed', 'mouth', 'person', 'horse', 'snake', 'jar', 'spoon', 'lamp', 'kite', 'monkey', 'swing', 'cloud', 'snowman', 'baby', 'eyes', 'pen', 'giraffe', 'grapes', 'book', 'ocean', 'star', 'cupcake', 'cow', 'lips', 'worm', 'sun', 'basketball', 'hat', 'bus', 'chair', 'purse', 'head', 'spider','shoe', 'ghost', 'coat', 'chicken', 'heart', 'jellyfish', 'tree', 'seashell', 'duck', 'bracelet', 'grass', 'jacket', 'slide', 'doll', 'spider', 'clock', 'cup', 'bridge', 'apple', 'balloon', 'drum', 'ears', 'egg', 'bread', 'nose', 'house', 'beach', 'airplane', 'inchworm', 'hippo', 'light', 'turtle', 'ball', 'carrot', 'cherry', 'ice', 'pencil', 'circle', 'bed', 'ant', 'girl', 'glasses', 'flower', 'mouse', 'banana', 'alligator', 'bell', 'robot', 'smile', 'bike', 'rocket', 'dinosaur', 'dog', 'bunny', 'cookie', 'bowl', 'apple', 'door', 'horse', 'door', 'song', 'trip', 'backbone', 'bomb', 'round', 'treasure', 'garbage', 'park', 'whistle', 'palace', 'baseball', 'coal', 'queen', 'dominoes', 'photograph', 'computer', 'hockey', 'aircraft', 'pepper', 'key', 'ipad', 'whisk', 'cake', 'circus', 'battery', 'mailman', 'cowboy', 'password', 'bicycle', 'skate', 'electricity', 'lightsaber', 'nature', 'shallow', 'toast', 'outside', 'America', 'roller', 'blading', 'gingerbread', 'man', 'bowtie', 'light', 'bulb', 'platypus', 'music', 'sailboat', 'popsicle', 'knee', 'pineapple', 'tusk', 'sprinkler','money', 'spool', 'lighthouse', 'doormat', 'face', 'flute', 'owl', 'gate', 'suitcase', 'bathroom', 'scale', 'peach', 'newspaper', 'watering', 'can', 'hook', 'school', 'beaver', 'camera', 'hair', 'dryer', 'mushroom', 'quilt', 'chalk', 'dollar', 'soda', 'chin', 'swing', 'garden','ticket', 'boot', 'cello', 'rain', 'clam', 'pelican', 'stingray', 'nail', 'sheep', 'stoplight', 'coconut', 'crib', 'hippopotamus', 'ring', 'video', 'camera', 'snowflake'];
+var currentPoints = 0;
+var guessedCorrect = false;
 function preload() {
     tenSecondsLeft = loadSound("sounds/countdownSound.mp3");
     timesup = loadSound("sounds/airhorn.mp3");
@@ -23,37 +27,43 @@ function preload() {
 function setup() {
     w = 1400;
     h = 750;
+    counter = 0;
     createCanvas(w, h);
     noStroke();
     rect(0, 0, w, h);
     stroke(0);
     strokeWeight(5);
     frameRate(60);
-    var words = ['cheese', 'bone', 'socks', 'leaf', 'whale', 'pie', 'shirt', 'orange', 'lollipop', 'bed', 'mouth', 'person', 'horse', 'snake', 'jar', 'spoon', 'lamp', 'kite', 'monkey', 'swing', 'cloud', 'snowman', 'baby', 'eyes', 'pen', 'giraffe', 'grapes', 'book', 'ocean', 'star', 'cupcake', 'cow', 'lips', 'worm', 'sun', 'basketball', 'hat', 'bus', 'chair', 'purse', 'head', 'spider','shoe', 'ghost', 'coat', 'chicken', 'heart', 'jellyfish', 'tree', 'seashell', 'duck', 'bracelet', 'grass', 'jacket', 'slide', 'doll', 'spider', 'clock', 'cup', 'bridge', 'apple', 'balloon', 'drum', 'ears', 'egg', 'bread', 'nose', 'house', 'beach', 'airplane', 'inchworm', 'hippo', 'light', 'turtle', 'ball', 'carrot', 'cherry', 'ice', 'pencil', 'circle', 'bed', 'ant', 'girl', 'glasses', 'flower', 'mouse', 'banana', 'alligator', 'bell', 'robot', 'smile', 'bike', 'rocket', 'dinosaur', 'dog', 'bunny', 'cookie', 'bowl', 'apple', 'door', 'horse', 'door', 'song', 'trip', 'backbone', 'bomb', 'round', 'treasure', 'garbage', 'park', 'whistle', 'palace', 'baseball', 'coal', 'queen', 'dominoes', 'photograph', 'computer', 'hockey', 'aircraft', 'pepper', 'key', 'ipad', 'whisk', 'cake', 'circus', 'battery', 'mailman', 'cowboy', 'password', 'bicycle', 'skate', 'electricity', 'lightsaber', 'nature', 'shallow', 'toast', 'outside', 'America', 'roller', 'blading', 'gingerbread', 'man', 'bowtie', 'light', 'bulb', 'platypus', 'music', 'sailboat', 'popsicle', 'knee', 'pineapple', 'tusk', 'sprinkler','money', 'spool', 'lighthouse', 'doormat', 'face', 'flute', 'owl', 'gate', 'suitcase', 'bathroom', 'scale', 'peach', 'newspaper', 'watering', 'can', 'hook', 'school', 'beaver', 'camera', 'hair', 'dryer', 'mushroom', 'quilt', 'chalk', 'dollar', 'soda', 'chin', 'swing', 'garden','ticket', 'boot', 'cello', 'rain', 'clam', 'pelican', 'stingray', 'nail', 'sheep', 'stoplight', 'coconut', 'crib', 'hippopotamus', 'ring', 'video', 'camera', 'snowflake'];
-    randomWord = words[getRandomInt(0, words.length-1)].toString();
-    timer = select("#timer");
-    word = select('#randomWord');
+    var timer = select("#timer");
+    var word = select('#randomWord');
     timer.html(convertSeconds(timeleft - counter));
-    word.html(randomWord);
+    word.html(getRandomWord());
     interval = setInterval(timeIt, 1000);
     function timeIt() {
         counter++;
         timer.html(convertSeconds(timeleft - counter));
-        if(counter === timeleft) {
+        print(((document.getElementById("guess").value === randomWord) && (pressed === true)));
+        if((counter >= timeleft) || guessedCorrect){
+            timer.style("color", "#FF0000");
+            setTimeout(function(){timer.style("color", "#FFFFFF")}, 4000);
             timesup.play();
             clearInterval(interval);
+            counter = timeleft;
+            setTimeout(function(){reset();interval = setInterval(timeIt, 1000);guessedCorrect = false;}, 3000);
+            document.getElementById("guess").value = ""
         }
         if(counter >= timeleft-10 && counter < timeleft) {
             tenSecondsLeft.play();
             timer.style("color", "#FF0000");
             setTimeout(function(){timer.style("color", "#FFFFFF")}, 500)
         }
+
     }
     colorNow = select("#colorSelected");
 }
 
 function draw() {
-    if (mouseIsPressed && counter !== 0) {
+    if (mouseIsPressed && counter !== timeleft) {
         line(mouseX, mouseY, pmouseX, pmouseY);
     }
 }
@@ -64,6 +74,11 @@ function convertSeconds(s) {
     return nf(minutes,2)+":"+nf(seconds,2);
 }
 
+function getRandomWord(){
+    randomWord = words[getRandomInt(0, words.length-1)].toString();
+    return randomWord
+}
+
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -71,26 +86,23 @@ function getRandomInt(min, max) {
 }
 
 function takeGuess(){
-    let guess = document.getElementById("guess").value;
-    var currentPoints = 0;
     points = select("#score");
     points.html(currentPoints);
-    if(guess === randomWord) {
+    if(document.getElementById("guess").value === randomWord) {
         document.getElementById("guess").style.backgroundColor = "#00FF00";
         document.getElementById("guess").value ="";
         ding.play();
-        clearInterval(interval);
-        currentPoints = currentPoints + 10;
+        currentPoints += 10;
         points.html(currentPoints);
+        guessedCorrect = true;
     }
     else {
         document.getElementById("guess").style.backgroundColor = "#FF0000";
-        document.getElementById("guess").value ="";
+        document.getElementById("guess").value = "";
         buzz.play();
         setTimeout(function(){document.getElementById("guess").style.backgroundColor = "#FFFFFF"}, 1000);
     }
-}
-
+    }
 
 
 function clearBoard(){
@@ -120,7 +132,6 @@ function User(){
     }
 }
 
-
 function drawer(){
     if (i >= list.length){ // is i > 3?
         i = 0;
@@ -130,7 +141,14 @@ function drawer(){
     i++
 }
 
+function isPressed() {
+    pressed = true;
+}
 function reset(){
+    clearBoard();
+    counter = -1;
+    word = select('#randomWord');
+    word.html(getRandomWord());
     for(var x in list){
         list[x].artist = false;
     }
